e7641241e54cc792bffbe8d437b80be3f5e8dd11
Echo <1515709028@qq.com>
2024-09-17 23:42:54 +0800

    修正了application中的问题，springboot大概正常了吧

--- src/main/java/com/lib/lib/Main.java
+++ src/main/java/com/lib/lib/Main.java
@@ -1,10 +1,19 @@
 package com.lib.lib;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.beans.factory.annotation.Value;
 
 @SpringBootApplication
-public class Main {
+public class Main implements CommandLineRunner {
+    @Value("${server.port}")
+    private String serverport;
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Server port: " + serverport+"is running");
     }
 }
