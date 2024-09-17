75baccf36a0a4faf3ff1fde16ff08d4c0eb9dbff
Echo <1515709028@qq.com>
2024-09-17 23:14:50 +0800

    项目升级为springboot架构

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,12 @@
 module com.lib.lib {
+
     requires javafx.controls;
     requires javafx.fxml;
-
-
-    opens com.lib.lib to javafx.fxml;
+    requires spring.boot.autoconfigure;
+    requires spring.boot;
+    requires spring.context;
+    requires spring.core;
+    requires spring.beans;
+    opens com.lib.lib to javafx.fxml,spring.core,spring.boot,spring.context,spring.beans;
     exports com.lib.lib;
 }
